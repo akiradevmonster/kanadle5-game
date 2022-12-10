7503f3ee6b33f12f3eb807d420ffeec69493c8e7
Akira Yamamoto <devmonster2417@gmail.com>
2022-12-10 09:58:39 +0900

    hotfix: change main logic of index.ts

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Components barrel file
+ * Export all components from this file
+ */
+
+export { default as GameBoard } from './GameBoard';
+export { default as HiraganaKeyboard } from './HiraganaKeyboard';
